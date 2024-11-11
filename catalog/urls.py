35aa25297e8cb60aef9d6d884e098024a89455e4
Aleksander <sanya.slepow2015@yandex.ru>
2024-11-12 00:12:35 +0500

    Dc13

---
+++ catalog/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from catalog.apps import CatalogConfig
+from catalog.views import HomeTemplateView, ProductDetailsView, ProductListView, contacts
+
+
+app_name = CatalogConfig.name
+
+urlpatterns = [
+    path('home/', HomeTemplateView.as_view(), name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('', ProductListView.as_view(), name='products_list'),
+    path('product/<int:pk>/', ProductDetailsView.as_view(), name='product')
+]
